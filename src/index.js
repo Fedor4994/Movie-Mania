fb49df510b1b7209a705a3f55bcf45aae92c9ce0
Fedor4994 <fedorsosnin04@gmail.com>
2022-12-20 12:52:37 +0200

    add basename for gh-pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="Movies">
+    <BrowserRouter basename="react-router-filmoteka">
       <App />
     </BrowserRouter>
   </React.StrictMode>
